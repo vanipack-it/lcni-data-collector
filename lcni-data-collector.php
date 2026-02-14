58fafa300cd36a7e5daecdbbe885e302e027ad5b
vanipack-it <kimlong.vanipack@gmail.com>
2026-02-14 13:26:19 +0700

    Create lcni-data-collector.php

---
+++ lcni-data-collector.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: LCNI Data Collector
+Description: Lưu dữ liệu giá từ DNSE API
+Version: 1.0
+*/
+
+if (!defined('ABSPATH')) exit;
+
+define('LCNI_PATH', plugin_dir_path(__FILE__));
+
+require_once LCNI_PATH . 'includes/class-lcni-db.php';
+require_once LCNI_PATH . 'includes/class-lcni-settings.php';
+require_once LCNI_PATH . 'includes/class-lcni-api.php';
+
+register_activation_hook(__FILE__, ['LCNI_DB', 'create_tables']);
+
+new LCNI_Settings();
